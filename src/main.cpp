81e0c9d88a078e4ee71b5b42b6491bfba7faae1f
Vlada <vladapopov02@gmail.com>
2023-05-22 17:29:59 +0200

    feat: Finish parallel implementation

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,17 @@
 #include <algorithm>
 #include <iostream>
 #include <chrono>
+#include "tbb/task_group.h"
+#include "tbb/parallel_for.h"
+#include "tbb/blocked_range.h"
+#include "tbb/concurrent_vector.h"
 
 // population size has to be even
 #define POPULATION_SIZE 500
-#define BOARD_SIZE 20
+#define BOARD_SIZE 60
 
 using namespace std;
+using namespace tbb;
 
 random_device device;
 mt19937 rng(device());
@@ -104,51 +109,93 @@ pair<vector<int>, vector<int>> crossover(int n, vector<int> parent1, vector<int>
     return make_pair(child1, child2);
 }
 
+vector<pair<vector<int>, int>> next_generation(vector<pair<vector<int>, int>> &population_scores, int start, int end, int n)
+{
+    vector<vector<int>> children;
+    end = (population_scores.size() < end ? population_scores.size() : end);
+    for (int i = start; i < end; i++)
+    {
+        // sortiranje po prilagodjenosti - selekcija
+        vector<pair<vector<int>, double>> population_scores_roulette;
+        transform(population_scores.begin(), population_scores.end(), back_inserter(population_scores_roulette), [&](pair<vector<int>, int> &ind)
+                  { return make_pair(ind.first, ind.second * get_random_real(0.0, 1.0)); });
+        sort(population_scores_roulette.begin(), population_scores_roulette.end(), [](const pair<vector<int>, double> &ind1, const pair<vector<int>, double> &ind2)
+             { return ind1.second < ind2.second; });
+
+        vector<int> child1, child2;
+        tie(child1, child2) = crossover(n, population_scores_roulette[0].first, population_scores_roulette[1].first);
+        children.push_back(child1);
+        children.push_back(child2);
+    }
+
+    vector<pair<vector<int>, int>> children_scores;
+    transform(children.begin(), children.end(), back_inserter(children_scores), [&](vector<int> &ind)
+              { return make_pair(ind, fitness_score(n, ind)); });
+    sort(children_scores.begin(), children_scores.end(), [](const pair<vector<int>, int> &ind1, const pair<vector<int>, int> &ind2)
+         { return ind1.second < ind2.second; });
+
+    // clear children to avoid reallocating memory
+    children.clear();
+
+    return children_scores;
+}
+
 pair<vector<int>, int> genetic(int n, int population_size)
 {
+    int group_size;
+
+    if (n < 16)
+    {
+        group_size = 20;
+    }
+    else if (n < 30)
+    {
+        group_size = 10;
+    }
+    else if (n < 60)
+    {
+        group_size = 5;
+    }
+    else
+    {
+        group_size = 1;
+    }
+
     int generation = 0;
-    int counter = 0;
 
     // inicijalizacija - kodiranje jedinki
     vector<vector<int>> population = init(n, population_size);
     // skladistimo fitness_score negde pa zip
-    vector<pair<vector<int>, int>> population_scores;
-    transform(population.begin(), population.end(), back_inserter(population_scores), [&](vector<int> &ind)
-              { return make_pair(ind, fitness_score(n, ind)); });
+    vector<pair<vector<int>, int>> population_scores(population.size());
+    parallel_for(size_t(0), population.size(), [&](size_t i)
+                 { population_scores[i] = make_pair(population[i], fitness_score(n, population[i])); });
+    // transform(population.begin(), population.end(), back_inserter(population_scores), [&](vector<int> &ind)
+    //           { return make_pair(ind, fitness_score(n, ind)); });
+    task_group g;
 
     while (true)
     {
-        counter += 1;
-        // pravljenje dece - ukrstanje
-        vector<vector<int>> children;
-        for (int i = 0; i < population_size / 2; i++)
-        {
-            // sortiranje po prilagodjenosti - selekcija
-            vector<pair<vector<int>, double>> population_scores_roulette;
-            transform(population_scores.begin(), population_scores.end(), back_inserter(population_scores_roulette), [&](pair<vector<int>, int> &ind)
-                      { return make_pair(ind.first, ind.second * get_random_real(0.0, 1.0)); });
-            sort(population_scores_roulette.begin(), population_scores_roulette.end(), [](const pair<vector<int>, double> &ind1, const pair<vector<int>, double> &ind2)
-                 { return ind1.second < ind2.second; });
+        generation++;
+        concurrent_vector<vector<pair<vector<int>, int>>> parallel_res;
 
-            vector<int> child1, child2;
-            tie(child1, child2) = crossover(n, population_scores_roulette[0].first, population_scores_roulette[1].first);
-            children.push_back(child1);
-            children.push_back(child2);
-        }
-
-        generation += 1;
-
-        // pustamo 5% najboljih roditelja da prezive - elitizam
-        int elitism_deg = population_size * 0.05;
+        parallel_for(blocked_range<int>(0, population_size), [&](blocked_range<int> range)
+                     {
+                    for(int i = range.begin(); i < range.end(); i+=group_size) {
+                        g.run([&] { parallel_res.push_back(next_generation(population_scores, i, i + group_size, n));});
+                    } });
+        g.wait();
 
         vector<pair<vector<int>, int>> children_scores;
-        transform(children.begin(), children.end(), back_inserter(children_scores), [&](vector<int> &ind)
-                  { return make_pair(ind, fitness_score(n, ind)); });
-        sort(children_scores.begin(), children_scores.end(), [](const pair<vector<int>, int> &ind1, const pair<vector<int>, int> &ind2)
-             { return ind1.second < ind2.second; });
+
+        for (int i = 0; i < parallel_res.size(); ++i)
+        {
+            children_scores.insert(children_scores.end(), make_move_iterator(parallel_res[i].begin()), make_move_iterator(parallel_res[i].end()));
+        }
+
         sort(population_scores.begin(), population_scores.end(), [](const pair<vector<int>, int> &ind1, const pair<vector<int>, int> &ind2)
              { return ind1.second < ind2.second; });
-
+        // pustamo 5% najboljih roditelja da prezive - elitizam
+        int elitism_deg = population_size * 0.05;
         for (int i = 0; i < elitism_deg; i++)
         {
             if (children_scores[children_scores.size() - i - 1].second > population_scores[i].second)
@@ -160,7 +207,7 @@ pair<vector<int>, int> genetic(int n, int population_size)
         sort(children_scores.begin(), children_scores.end(), [](const pair<vector<int>, int> &ind1, const pair<vector<int>, int> &ind2)
              { return ind1.second < ind2.second; });
         int best_child = children_scores[0].second;
-        cout << "\r" << counter << ". " << best_child << flush;
+        cout << "\r" << generation << ". " << best_child << flush;
         if (best_child == 0)
         {
             cout << endl;
@@ -169,9 +216,6 @@ pair<vector<int>, int> genetic(int n, int population_size)
 
         transform(children_scores.begin(), children_scores.end(), population_scores.begin(), [](const pair<vector<int>, int> &ind)
                   { return ind; });
-
-        // clear children to avoid reallocating memory
-        children.clear();
     }
 }
 

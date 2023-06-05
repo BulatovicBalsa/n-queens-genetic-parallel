a797cc20cfd8a56c8df20c6929c1899a218d4e5d
Vlada <vladapopov02@gmail.com>
2023-06-05 22:56:09 +0200

    feat: Add result check at the end of each algorithm variant

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include "algorithm.hpp"
 
 // population size has to be even
-#define POPULATION_SIZE 500
+#define POPULATION_SIZE 300
 #define BOARD_SIZE 60
 
 using namespace std;
@@ -28,6 +28,15 @@ int main()
     auto duration = chrono::duration_cast<chrono::seconds>(stop - start);
     cout << "Vreme izvrsavanja paralelnog algoritma: " << duration.count() << "s" << endl;
     cout << "Vreme po generaciji paralelnog algoritma: " << duration.count() / (generations * 1.0) << "s" << endl;
+    if (fitness_score(BOARD_SIZE, optimal_solution) == 0)
+    {
+        cout << "Resenje tacno!" << endl;
+    }
+    else
+    {
+        cout << "Resenje nije tacno!" << endl;
+    }
+    cout << endl;
 
     start = chrono::high_resolution_clock::now();
     tie(optimal_solution, generations) = genetic_serial(BOARD_SIZE, POPULATION_SIZE);
@@ -40,6 +49,14 @@ int main()
     duration = chrono::duration_cast<chrono::seconds>(stop - start);
     cout << "Vreme izvrsavanja serijskog algoritma: " << duration.count() << "s" << endl;
     cout << "Vreme po generaciji serijskog algoritma: " << duration.count() / (generations * 1.0) << "s" << endl;
+    if (fitness_score(BOARD_SIZE, optimal_solution) == 0)
+    {
+        cout << "Resenje tacno!" << endl;
+    }
+    else
+    {
+        cout << "Resenje nije tacno!" << endl;
+    }
 
     return 0;
 }
